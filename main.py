72779751381049b32db275556ba0ea648d3a6909
Akash kr Singha <akashkumarsingha08@gmail.com>
2026-06-30 21:30:29 +0530

    fix chapter1-menu new domain in cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000","https://chapter1-frontend-lppv.vercel.app" ],             
+    allow_origins = ["http://localhost:3000","https://chapter1-menu-lppv.vercel.app" ],             
     allow_methods=["*"],
     allow_headers=["*"])
 
